aeac45b5934dc933099e134099f0ba22058573af
Oleksandr Petryk <sachapos50@gmail.com>
2023-02-28 23:18:40 +0200

    feat: create index.ts for lambda and CI/CD action

---
+++ index.ts
@@ -0,0 +1,13 @@
+import {
+  Handler,
+  // APIGatewayProxyEvent,
+  APIGatewayProxyResultV2,
+} from 'aws-lambda';
+
+export const handler: Handler = async (): // _: APIGatewayProxyEvent,
+Promise<APIGatewayProxyResultV2> => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+};
